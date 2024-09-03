9b7a24f1002c3a0367e5e6d38db387ac50c01128
Tinalexman <taiwoluwatobilobafestus@gmail.com>
2024-09-03 23:54:42 +0100

    Progress

--- src/components/dashboard/download/Download.tsx
+++ src/components/dashboard/download/Download.tsx
@@ -21,33 +21,33 @@ const Download = () => {
             Generate Statement
           </h2>
           <div className="flex flex-col gap-2 md:gap-1 w-full">
-            <h3 className="text-body text-neutral-2">Date</h3>
+            <h3 className="text-b-2 text-neutral-2">Date</h3>
             <div className="flex w-full justify-between items-center">
               <input
                 type="date"
                 value={start}
                 placeholder="From"
                 onChange={(e) => setStart(e.target.value)}
-                className="h-12 md:h-10 rounded-full w-[49%]"
+                className="rounded-full w-[48%] text-b-1"
               />
               <input
                 type="date"
                 value={end}
                 placeholder="To"
                 onChange={(e) => setEnd(e.target.value)}
-                className="h-12 rounded-full w-[49%]"
+                className="rounded-full w-[48%] text-b-1"
               />
             </div>
           </div>
           <div className="flex flex-col gap-2 md:gap-1 w-full">
-            <h3 className="text-body text-neutral-2">Email</h3>
+            <h3 className="text-b-2 text-neutral-2">Email</h3>
             <input
               type="email"
               name="email"
               placeholder="Enter your email address"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
-              className="w-full text-body rounded-full h-12"
+              className="w-full text-b-1 rounded-full"
             />
           </div>
         </>
@@ -74,7 +74,7 @@ const Download = () => {
             isSent(true);
           }
         }}
-        className={`bg-primary rounded-full lg:w-[60%] xs:w-full text-body h-12 md:h-10 md:mb-20 text-white font-bold mt-5`}
+        className={`bg-primary rounded-full lg:w-[70%] xs:w-full text-l-1 lg:h-12 xs:h-10  text-white font-semibold mt-5`}
       >
         {sent ? "Back to Home" : "Send Receipt"}
       </button>
